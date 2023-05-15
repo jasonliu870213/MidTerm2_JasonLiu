1cd93560b7e5bc89203747b01ed8e451396135cf
jasonliu870213 <jasonliu870213@gmail.com>
2023-05-15 21:29:45 +0800

    - hand in no.2

--- two/main.py
+++ two/main.py
@@ -4,13 +4,13 @@ session = requests.session()
 body = {
    "firstName": "Jason",
    "lastName": "Liu",
-   "email": "jason4@gmail.com"
+   "email": "jason33@gmail.com"
 }
 r = session.request("post", f'{URL}/user/create', json=body, headers={'app-id': '63eb435ad8bb0b7ee5b67a77'})
 
 print(r.json())
-id = r.json()["id"]
-print('id: ',id)
+user_id = r.json()["id"]
+print('id: ', user_id)
 
 body = {
     "text": "test",
@@ -20,4 +20,5 @@ body = {
     "owner": id
 }
 r = session.request("post", f'{URL}/post/create', json=body, headers={'app-id': '63eb435ad8bb0b7ee5b67a77'})
-print(r)
+post_id = r.json()['id']
+print('post id:', post_id)
