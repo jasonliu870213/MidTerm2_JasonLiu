244e88092981f4cd4d62e57c0b470bbfdda99db1
jasonliu870213 <jasonliu870213@gmail.com>
2023-05-15 21:25:21 +0800

    - hand in no.2

--- two/main.py
+++ two/main.py
@@ -11,3 +11,13 @@ r = session.request("post", f'{URL}/user/create', json=body, headers={'app-id': 
 print(r.json())
 id = r.json()["id"]
 print('id: ',id)
+
+body = {
+    "text": "test",
+    "image": "string(url)",
+    "likes": "30",
+    "tags": "array(string)",
+    "owner": "string(User id)"
+}
+r = session.request("post", f'{URL}/post/create', json=body, headers={'app-id': '63eb435ad8bb0b7ee5b67a77'})
+
